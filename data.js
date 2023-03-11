fc209f6aafce8bf78c6dcdbcbb0637375343ae91
MetehanBayhan <metehnb@gmail.com>
2023-03-11 16:28:35 +0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,33 @@
+export const gifData = [
+    {
+        topic: ["depresive"],
+        gif: "gifs/donen-recep.gif",
+        alt: "Recep İvedik dönüyor",
+    },
+    {
+        topic: ["grateful"],
+        gif: "gifs/eyvallah-eyv.gif",
+        alt: "Recep Ivedik showing respect",
+    },
+    {
+        topic: ["model"],
+        gif: "gifs/q-we.gif",
+        alt: "Recep İvedik making pose",
+    },
+    {
+        topic: ["laughing"],
+        gif: "gifs/recep-recep-ivedik.gif",
+        alt: "Recep İvedik laughing",
+    },
+    {
+        topic: ["fisherman"],
+        gif: "gifs/say-lan.gif",
+        alt: "Recep İvedik diver",
+    },
+    {
+        topic: ["athlete"],
+        gif: "gifs/tayt.gif",
+        alt: "Recep İvedik wearing tight",
+    },
+
+]
---
+++ index.js
@@ -0,0 +1,77 @@
+import { gifData } from "/data.js"
+
+const moodListDiv = document.getElementById('mood-list-div')
+const memeModal = document.getElementById('meme-modal')
+const getGifBtn = document.getElementById('get-gif')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const closeBtn = document.getElementById('meme-modal-close-btn')
+
+moodListDiv.addEventListener('change', highlightCheckedOption)
+
+closeBtn.addEventListener('click', closeGif)
+
+function closeGif() {
+    memeModal.style.display = 'none'
+}
+function highlightCheckedOption(e) {
+    const radios = document.getElementsByClassName('radio')
+    for (let radio of radios) {
+        radio.classList.remove('highlight')
+    }
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
+function getMatched() {
+    if(document.querySelector('input[type="radio"]:checked')) {
+        const currentMood = document.querySelector('input[type="radio"]:checked').value
+        const findMood =  gifData.filter(function(mood){
+            return mood.topic.includes(currentMood)
+        })
+        return findMood
+    }
+}
+
+
+function getGif(){
+    const gifArray = getMatched()
+    return gifArray[0]
+}
+
+getGifBtn.addEventListener('click', render)
+
+function render(){
+    const gif = getGif()
+    memeModalInner.innerHTML = `
+    <img 
+    class="gif" 
+    src="${gif.gif}"
+    alt="${gif.alt}"
+    >
+    `
+    memeModal.style.display = 'flex'
+}
+
+
+function getTopicsFromArray(arr) {
+    let topicList = []
+    for (let object of arr){
+        topicList.push(object.topic[0])
+    }
+    return topicList
+}
+
+function getMoodRadios(arr) {
+    let radios = ``
+    const getMoods = getTopicsFromArray(arr)
+    for (let mood of getMoods) {
+        radios += `
+        <div class="radio">
+        <label for="${mood}">${mood}</label>
+        <input type="radio" id="${mood}" value="${mood}" name="moodies">
+        </div>`
+    }
+    moodListDiv.innerHTML = radios
+
+}
+
+getMoodRadios(gifData)
